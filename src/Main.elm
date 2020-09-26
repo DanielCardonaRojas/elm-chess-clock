549e3f20f39eda46cc82e3d598ae049d9af90cb0
Daniel Cardona Rojas <daniel.cardonarojas@endava.com>
2020-09-26 18:43:54 -0500

    Refactor using Return package

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,9 @@ import Data.Player as Player exposing (Player(..))
 import Model exposing (..)
 import Msg exposing (..)
 import Platform.Cmd as Cmd
+import Return
 import Time
+import TypedTime
 import Utils exposing (iff)
 import View exposing (view)
 
@@ -21,7 +23,7 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model.initialModel, Cmd.none )
+    ( Model.initialModel 3, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -40,30 +42,19 @@ update msg model =
             ( model, Cmd.none )
 
         SwitchPlayer ->
-            ( { model
-                | turn = Player.switch model.turn
-                , player1MoveCount = iff (model.turn == Player1) (model.player1MoveCount + 1) model.player1MoveCount
-                , player2MoveCount = iff (model.turn == Player2) (model.player2MoveCount + 1) model.player2MoveCount
-              }
-            , Cmd.none
-            )
+            Return.singleton model
+                |> Return.map Model.timeCompensation
+                |> Return.map Model.incrementMoves
+                |> Return.map (\m -> { m | turn = Player.switch m.turn })
 
         Tick ->
-            ( { model
-                | player1Ticks =
-                    iff (model.turn == Player1) (model.player1Ticks + 1) model.player1Ticks
-                , player1PartialTicks =
-                    iff (model.turn == Player1) (model.player1PartialTicks + 1) 0
-                , player2Ticks =
-                    iff (model.turn == Player2) (model.player2Ticks + 1) model.player2Ticks
-                , player2PartialTicks =
-                    iff (model.turn == Player2) (model.player2PartialTicks + 1) 0
-              }
-            , Cmd.none
-            )
+            Return.singleton model
+                |> Return.map Model.incrementTime
 
         Pause ->
-            ( { model | paused = True }, Cmd.none )
+            Return.singleton model
+                |> Return.map (\m -> { m | paused = True })
 
         Resume ->
-            ( { model | paused = False }, Cmd.none )
+            Return.singleton model
+                |> Return.map (\m -> { m | paused = False })
--- src/Model.elm
+++ src/Model.elm
@@ -2,60 +2,109 @@ module Model exposing (..)
 
 import Data.ClockMode exposing (ClockMode, TimeControl(..))
 import Data.Player exposing (Player(..))
+import String exposing (fromInt)
 import TypedTime exposing (TypedTime, Unit(..), milliseconds, seconds)
 import Utils exposing (iff)
 
 
 type alias Model =
     { turn : Player
-    , player1Ticks : Int -- Total amount of clock ticks accumulated
-    , player2Ticks : Int
-    , player1PartialTicks : Int -- Amount of clock ticks accumulated until moves
-    , player2PartialTicks : Int
+    , player1Time : TypedTime -- Total amount of clock ticks accumulated
+    , player2Time : TypedTime
+    , player1PartialTime : TypedTime -- Amount of clock ticks accumulated until moves
+    , player2PartialTime : TypedTime
     , player1MoveCount : Int
     , player2MoveCount : Int
     , clockMode : ClockMode
     , tickLength : TypedTime
-    , totalTime : TypedTime
     , paused : Bool
     }
 
 
-initialModel : Model
-initialModel =
+initialModel : Int -> Model
+initialModel minutes =
     { turn = Player1
-    , player1Ticks = 0
-    , player2Ticks = 0
-    , player1PartialTicks = 0
-    , player2PartialTicks = 0
+    , player1Time = TypedTime.minutes <| toFloat minutes
+    , player2Time = TypedTime.minutes <| toFloat minutes
+    , player1PartialTime = TypedTime.zero
+    , player2PartialTime = TypedTime.zero
     , player1MoveCount = 0
     , player2MoveCount = 0
     , clockMode = { timeControl = Fisher, compensation = TypedTime.seconds 5 }
     , tickLength = TypedTime.milliseconds 100
-    , totalTime = TypedTime.seconds 40
     , paused = False
     }
 
 
 remainingTime : Model -> Player -> String
 remainingTime model player =
-    let
-        convertToString time =
-            if TypedTime.lt time (seconds 30) then
-                TypedTime.toString Milliseconds time
-
-            else
-                TypedTime.toString Milliseconds time
-
-        calculate ticks partialTicks =
-            TypedTime.multiply (toFloat ticks) model.tickLength
-                |> TypedTime.sub model.totalTime
-                |> iff (model.clockMode.timeControl == Fisher) (TypedTime.add model.clockMode.compensation) identity
-                |> convertToString
-    in
     case player of
         Player1 ->
-            calculate model.player1Ticks model.player1PartialTicks
+            TypedTime.toString Seconds model.player1Time
 
         Player2 ->
-            calculate model.player2Ticks model.player2PartialTicks
+            TypedTime.toString Seconds model.player2Time
+
+
+incrementMoves : Model -> Model
+incrementMoves model =
+    { model
+        | player1MoveCount = iff (model.turn == Player1) (model.player1MoveCount + 1) model.player1MoveCount
+        , player2MoveCount = iff (model.turn == Player2) (model.player2MoveCount + 1) model.player2MoveCount
+    }
+
+
+incrementTime : Model -> Model
+incrementTime model =
+    { model
+        | player1Time =
+            iff (model.turn == Player1) (TypedTime.sub model.player1Time model.tickLength) model.player1Time
+        , player1PartialTime =
+            iff (model.turn == Player1) (TypedTime.add model.player1PartialTime model.tickLength) TypedTime.zero
+        , player2Time =
+            iff (model.turn == Player2) (TypedTime.sub model.player2Time model.tickLength) model.player2Time
+        , player2PartialTime =
+            iff (model.turn == Player2) (TypedTime.add model.player2PartialTime model.tickLength) TypedTime.zero
+    }
+
+
+timeCompensation : Model -> Model
+timeCompensation model =
+    let
+        compensation =
+            case model.clockMode.timeControl of
+                Fisher ->
+                    TypedTime.add model.clockMode.compensation
+
+                _ ->
+                    TypedTime.add TypedTime.zero
+
+        apply player =
+            iff (model.turn == player) compensation identity
+    in
+    { model
+        | player1Time = apply Player1 model.player1Time
+        , player2Time = apply Player2 model.player2Time
+    }
+
+
+
+-- remainingTime : Model -> Player -> String
+-- remainingTime model player =
+--     let
+--         convertToString time =
+--             if TypedTime.lt time (seconds 30) then
+--                 TypedTime.toString Milliseconds time
+--             else
+--                 TypedTime.toString Milliseconds time
+--         calculate ticks partialTicks =
+--             TypedTime.multiply (toFloat ticks) model.tickLength
+--                 |> TypedTime.sub model.totalTime
+--                 |> iff (model.clockMode.timeControl == Fisher) (TypedTime.add model.clockMode.compensation) identity
+--                 |> convertToString
+--     in
+--     case player of
+--         Player1 ->
+--             calculate model.player1Ticks model.player1PartialTicks
+--         Player2 ->
+--             calculate model.player2Ticks model.player2PartialTicks
